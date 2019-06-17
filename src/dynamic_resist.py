63f6da0546fbb16a81c4977ff64e463f6c15a206
sdong001 <sdong001@gmail.com>
2019-06-18 04:12:00 +0900

    add sound, resist sensor value prcessing

--- src/dynamic_resist.py
+++ src/dynamic_resist.py
@@ -1,20 +1,44 @@
-import RPi.GPIO as GPIO
 import threading
+import spidev
+import time
 
 from const import Const
 from http_request import HttpRequest
 
 # HttpRequest example.
-# HttpRequest.request(Const.REQUEST_RESIST, value)
+# HttpRequest.request(Const.REQUEST_RESIST, value
 
+# value range : 0 ~ 4
 class DynamicResist(object):
     def __init__(self):
-        pass
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 1000000
+        self.mcp3008_channel = 0
+        self.delay = 0.5
+        self.city = -1
+
+    def read_channel(self, channel):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
+        return data
 
     def watch(self):
-        while True:
-            print('DynamicResist watching..')
-            pass
+        try:
+            while True:
+                adc_value = self.read_channel(self.mcp3008_channel)
+                city_value = int(5 * adc_value /1024)
+                if self.city == city_value:
+                    pass
+                else:
+                    self.city = city_value
+                    HttpRequest.request(Const.REQUEST_RESIST, self.city)
+
+                time.sleep(self.delay)
+        except KeyboardInterrupt:
+            self.spi.close()
+
+
 
     def run(self):
         t1 = threading.Thread(target=self.watch)
--- src/sensor_sound.py
+++ src/sensor_sound.py
@@ -1,5 +1,6 @@
-import RPi.GPIO as GPIO
 import threading
+import spidev
+import time
 
 from const import Const
 from http_request import HttpRequest
@@ -7,15 +8,29 @@ from http_request import HttpRequest
 # HttpRequest example.
 # HttpRequest.request(Const.REQUEST_WIND, value)
 
+# value range : 0 ~ 100
 
 class SensorSound(object):
     def __init__(self):
-        pass
+        self.spi = spidev.SpiDev()
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 1000000
+        self.mcp3008_channel = 1
+        self.delay = 0.2
+
+    def read_channel(self, channel):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
+        return data
 
     def watch(self):
         while True:
-            print('SensorSound watching..')
-            pass
+            adc_value = self.read_channel(self.mcp3008_channel)
+            if adc_value > 100:
+                calc_wind = int(100 * (adc_value-100) / 100)
+                wind_value = calc_wind > 100 and 100 or calc_wind
+                HttpRequest.request(Const.REQUEST_WIND, wind_value)
+            time.sleep(self.delay)
 
     def run(self):
         t1 = threading.Thread(target=self.watch)
